2dbd5454062366a7df8db20029756d11adaf2940
Yellow7Y <102256640+Yellow7Y@users.noreply.github.com>
2022-04-06 16:02:12 -0300

    Update exercicio4.java

--- src/com/company/exercicio4.java
+++ src/com/company/exercicio4.java
@@ -6,10 +6,12 @@ public class exercicio4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("DIGITE UM LETRA: ");
-        String letra = scanner.nextLine();
-        Boolean letraInformadaVogal = letra.equals("A") || letra.equals("a") || letra.equals("E") || letra.equals("e") || letra.equals("I") || letra.equals("i")
-                || letra.equals("O") || letra.equals("o") || letra.equals("U") || letra.equals("u");
+        String letraDigitada = scanner.nextLine();
+        Boolean letraInformadaVogal = letraDigitada.equals("A") || letraDigitada.equals("a") || letraDigitada.equals("E") || letraDigitada.equals("e") || letraDigitada.equals("I") ||
+                letraDigitada.equals("i") || letraDigitada.equals("O") || letraDigitada.equals("o") || letraDigitada.equals("U") || letraDigitada.equals("u");
         if (letraInformadaVogal)
             System.out.println("A LETRA QUE VOCE INFORMOU É UMA VOGAL!");
+        else
+            System.out.println("A LETRA QUE VOCE INFORMOU É UMA CONSOANTE!");
     }
 }
